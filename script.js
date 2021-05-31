f7e2df7b8132d77010cd713a74bc3aa67e7c4cf9
soop <>
2021-05-31 02:12:35 +0000

    nothing

---
+++ script.js
@@ -0,0 +1,17 @@
+var userinput = prompt("what do you want from me");
+var table = [["bruh", 3], ["shush", 69420], ["shut up", 69420666]]
+
+
+if(userinput == "shut up"); {
+  document.write("your such a noob");
+}
+if(userinput == "ratio"); {
+  document.write("wow ok, ratio");
+}
+if(userinput == "Goodbye!"); {
+  document.write("yeah that's write")
+}
+
+function noob() {
+  table.pop();
+}
